2120cbabe377805dc8fb7fe7daa3a7e3499b9852
Monica Butarbutar <monicabutarbutar04@gmail.com>
2023-05-26 21:16:36 +0700

    Initial Commit

---
+++ BlessedTeam/BlessedTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlessedTeamApp.swift
+//  BlessedTeam
+//
+//  Created by Monica Butarbutar on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BlessedTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

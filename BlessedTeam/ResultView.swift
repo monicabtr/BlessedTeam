de263ccaf5f428bba6573a8a3fbae81f85f2166f
wahyunandanicola <129938889+wahyunandanicola@users.noreply.github.com>
2023-06-11 18:22:12 +0700

    list timestamp di ResultView sudah berubah menjadi Button

--- BlessedTeam/ResultView.swift
+++ BlessedTeam/ResultView.swift
@@ -9,26 +9,65 @@ import SwiftUI
 import AVFoundation
 
 struct ResultView: View {
+    /////////////
+//    @State private var selectedTimestamp: String?
+    /////////////
     @Binding var timestamps: [String]
-    
     @State private var timer: Timer?
     @State private var elapsedTime: TimeInterval = 0
     @State private var isTimerRunning = false
     @StateObject var audioPlayer = AudioPlayer()
     @StateObject var audioRecorder = AudioRecorder()
+    ///////////////
+    struct GreenButtonStyle: ButtonStyle {
+        @State private var isPressed = false
+        
+        @State private var isButtonPressed = false
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(configuration.isPressed ? Color.green : Color.white)
+                .foregroundColor(configuration.isPressed ? Color.white : Color.primary)
+                .cornerRadius(15)
+                .onTapGesture {
+                    isPressed.toggle()
+             }
+        }
+    }
     
+    //////////////////////
     var body: some View {
         HStack {
             VStack {
                 Text("Tap to go to the time you want")
                     .font(.system(size: 20, weight: .bold))
-                List(timestamps, id: \.self) { timestamp in
+                //                List(timestamps, id: \.self) { timestamp in
+                //                    Text(timestamp)
+                //                        .font(.subheadline)
+                ////////////////////////////////////
+                //                    ForEach(timestamps, id: \.self) { timestamp in
+                //                                       Button(action: {
+                //                                           selectedTimestamp = timestamp
+                //                                       }) {
+                //                                           Text(timestamp)
+                //                                               .font(.subheadline)
+                //                                               .foregroundColor(selectedTimestamp == timestamp ? .black : .primary)
+                //                                       }
+                ////////////////////////////////////
+                ForEach(timestamps, id: \.self) { timestamp in
+                    Button(action: {
+                        // Action when the button is tapped
+                    }) {
                         Text(timestamp)
                             .font(.subheadline)
+                    }
+                    .buttonStyle(GreenButtonStyle())
+                    ///////////////////////////
+                }
+                
             }
             
-            }
-
             
             
             VStack{
@@ -38,6 +77,13 @@ struct ResultView: View {
                     .font(.system(size: 20, weight: .bold))
                 Text("00:03:01")
                     .font(.system(size: 20, weight: .bold))
+//                List(audioPlayer.recordings, id: \.self) { recordingURL in
+//                    Button(action: {
+//                        self.audioPlayer.playAudio(url: recordingURL)
+//                    }) {
+//                        Text(recordingURL.lastPathComponent)
+//                    }
+//                }
                 List(audioPlayer.recordings, id: \.self) { recordingURL in
                     Button(action: {
                         self.audioPlayer.playAudio(url: recordingURL)
